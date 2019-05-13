bf41856a383a0cb4c459eece6c3302a9f69fb120
Shubham Verma <VermaSh@users.noreply.github.com>
2019-05-13 13:48:17 -0400

    Fix for include and exclude options
    - Fixed include and exclude options, it should not be passed in as strings

--- sync_files.sh
+++ sync_files.sh
@@ -4,44 +4,43 @@
 #	- Want to do update only
 #	- Want to change source and destination directories
 
-#TODO:
-#	call file from bash_aliases with appropriate arguments
-#	use "-" instead of argument numbers
-
 # ./syncFiles.sh 'n' '' 'spec' $OMR_WORKSPACE $REMOTE_COMPILE_DIR
 # syncFiles.sh 'n' 'u' '' $OMR_WORKSPACE $REMOTE_COMPILE_DIR/omr
 # syncFiles.sh 'n' 'u' '' $OPENJ9_WORKSPACE $REMOTE_COMPILE_DIR
 
-machine="machine to ssh into"
+# $1 activate dry run
+# $2 toggle between update or overwrite
+# $3 append extra extensions to be included
+# $4 source directory
+# $5 destination directory
 
-mode='pushed'
-options="-ravz"
-if [ ! -z "$2" ] && [ "$2" = "u" ]; then
-	options=$options"u"
-	mode='updated'
-fi
-options=$options$1" --recursive --progress --prune-empty-dirs"
-
-filters="--include=*/ --include=*.cpp --include=*.hpp \
---include=*.c --include=*.h"
-extensions=".hpp, .cpp, .c, .h"
-if [ ! -z "$3" ]; then
-	filters=$filters" --include=*.$3"
-	extensions=$extensions", .$3"
-fi
-filters=$filters' '$extension" --exclude=* --exclude=.*"
-
-if [ ! -z "$6" ]; then
-	filters=$filters" --exclude=$6"
+if [ ! -z "$4" ] || [ ! -z "$5" ]; then
+	echo "Please provide a valid source and destination directory"
+	exit 1
 fi
 
 source=$4
 destination=$5
 
+machine= # machine to ssh into
+
+mode='pushed'
+options=-ravz
+
+if [ ! -z "$2" ] && [ "$2" = "u" ]; then
+	options=$options"u"
+	mode='updated'
+fi
+
+options=$options$1 --progress --prune-empty-dirs
+
+extensions='.hpp, .cpp, .c, .h'
+if [ ! -z "$3" ]; then
+	extensions=$extensions", .$3"
+fi
+
 printf "Only extensions $extensions will be $mode. \nSource      \
 : $source\nDestination : $destination\n"
 
-echo 'Filters     : '$filters
-echo 'Options     : '$options
-
-rsync $options "--exclude=.*" $source $machine:$destination
+rsync $options --include='*.'{cpp,hpp,c,h,$3} \
+	--exclude='*' \
